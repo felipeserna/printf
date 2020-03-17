eea06a6cdabaca374be190442b5bf9a80842c42e
Lucho-ai <56923543+Lucho-ai@users.noreply.github.com>
2020-03-17 11:35:18 -0500

    Create p_porcent.c

---
+++ p_porcent.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+
+/**
+ * p_porcent - Prints a percent
+ * @list: list of arguments                                                                                              * Return: Return the amount of characters printed.
+ */
+int p_porcent(__attribute__((unused))va_list list)
+{
+        _putchar('%');
+        return (1);
+}
