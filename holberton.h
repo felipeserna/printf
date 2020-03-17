ae54c1e1530bff4bea91916c3aef63f7e0cd7eed
Lucho-ai <56923543+Lucho-ai@users.noreply.github.com>
2020-03-17 12:45:44 -0500

    Create holberton.h

---
+++ holberton.h
@@ -0,0 +1,27 @@
+#ifndef PRINT_F
+#define PRINT_F
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+/*La struct functions sirve para definir una estructura
+para simbolos y funciones */
+
+struct functions
+{
+	char *sym;
+	int (*f)(va_list);
+};
+typedef struct functions func_t;
+
+int analyzer(const char *format, func_t f_list[], va_list arg_list);
+int _printf(const char *format, ...);
+int _putchar(char);
+int p_char(va_list);
+int p_string(va_list);
+int p_porcent(va_list);
+int p_interger(va_list);
+int p_number(va_list);
+int p_unsigned_int(va_list);
+
+#endif
