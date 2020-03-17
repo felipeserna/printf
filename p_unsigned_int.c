1337b18d68a036136f6852d74b36673f52fec80c
Lucho-ai <56923543+Lucho-ai@users.noreply.github.com>
2020-03-17 13:25:54 -0500

    Update p_unsigned_int.c

--- p_unsigned_int.c
+++ p_unsigned_int.c
@@ -7,11 +7,14 @@
  */
 int p_unsigned_int(va_list list)
 {
-        unsigned int uns;                                                                                                                                                                                                                               uns = va_arg(list, unsigned int);                                                                               
-        if (uns == 0)
-                return (p_unsigned_int(uns));
+	unsigned int uns;
 
-        if (uns < 1)
-                return (-1);
-        return (p_unsigned_int(uns));
+	uns = va_arg(list, unsigned int);
+
+	if (uns == 0)
+		return (p_unsigned_int(uns));
+
+	if (uns < 1)
+		return (-1);
+	return (p_unsigned_int(uns));
 }
