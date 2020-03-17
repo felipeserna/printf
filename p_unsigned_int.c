3601905807bf48ad55603a161abd186e2cd329f4
Lucho-ai <56923543+Lucho-ai@users.noreply.github.com>
2020-03-17 13:34:57 -0500

    Update p_unsigned_int.c

--- p_unsigned_int.c
+++ p_unsigned_int.c
@@ -1,20 +1,20 @@
 #include "holberton.h"
 
 /**
- * p_unsigned_int - Prints Unsigned integers
+ * p_unsigned_integer - Prints Unsigned integers
  * @list: List of all of the argumets
  * Return: Return a acount the numbers
  */
-int p_unsigned_int(va_list list)
+int p_unsigned_integer(va_list list)
 {
 	unsigned int num;
 
 	num = va_arg(list, unsigned int);
 
 	if (num == 0)
-		return (p_unsigned_int(num));
+		return (p_unsigned_integer(num));
 
 	if (num < 1)
 		return (-1);
-	return (p_unsigned_int(num));
+	return (p_unsigned_integer(num));
 }
