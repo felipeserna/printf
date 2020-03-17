bf683d8a240b227039cadf35d7ea754563e79ac7
Lucho-ai <56923543+Lucho-ai@users.noreply.github.com>
2020-03-17 12:25:29 -0500

    Create _printf.c

---
+++ _printf.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * _printf - Receives the main string and all the necessary parameters to
+ * print a formated string
+ * @format: All the desired characters
+ * Return: Total count of the characters printed
+ */
+int _printf(const char *format, ...)
+{
+	int printed;
+	func_t f_list[] = {
+		{"c", p_char},
+		{"s", p_string},
+		{"%", p_porcent},
+		{"d", p_integer},
+		{"i", p_integer},
+		{"u", p_unsigned_int},
+		{NULL, NULL}
+	};
+	va_list arg_list;
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(arg_list, format);
+	/*Calling parser function*/
+	printed = analyzer(format, f_list, arg_list);
+	va_end(arg_list);
+	return (printed);
+}
