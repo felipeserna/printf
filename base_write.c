dd7530d772d3c2023a71bf6558d0a6d04b93616d
Lucho-ai <56923543+Lucho-ai@users.noreply.github.com>
2020-03-17 15:13:07 -0500

    Create base_write.c

---
+++ base_write.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * write_base - sends characters to be written on standard output
+ * @str: String to parse
+ */
+void write_base(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		_write_char(str[i]);
+}
