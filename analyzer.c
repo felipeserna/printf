cd75ace03a906867285559d0f8ff1db58e77848a
Lucho-ai <56923543+Lucho-ai@users.noreply.github.com>
2020-03-17 12:18:41 -0500

    Create analyzer.c

---
+++ analyzer.c
@@ -0,0 +1,52 @@
+#include "holberton.h"
+
+/**
+ * analyzer - Receives the main string and all the necessary parameters to
+ * print a formated string.
+ * @format: A string containing all the desired characters.
+ * @f_list: A list of all the posible functions.
+ * @arg_list: A list containing all the argumentents passed to the program.
+ * Return: A total count of the characters printed.
+ */
+int analyzer(const char *format, func_t f_list[], va_list arg_list)
+{
+	int i, j, r_val, pr_chars;
+
+	pr_chars = 0;
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			/*Iterates through struct to find the right func*/
+			for (j = 0; f_list[j].sym != NULL; j++)
+			{
+				if (format[i + 1] == f_list[j].sym[0])
+				{
+					r_val = f_list[j].f(arg_list);
+					if (r_val == -1)
+						return (-1);
+					pr_chars += r_val;
+					break;
+				}
+			}
+			if (f_list[j].sym == NULL && format[i + 1] != ' ')
+			{
+				if (format[i + 1] != '\0')
+				{
+					_putchar(format[i]);
+					_putchar(format[i + 1]);
+					pr_chars = pr_chars + 2;
+				}
+				else
+					return (-1);
+			}
+			i = i + 1; /*Updating i to skip format symbols*/
+		}
+		else
+		{
+			_putchar(format[i]); /*call the write function*/
+			pr_chars++;
+		}
+	}
+	return (pr_chars);
+}
