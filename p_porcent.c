131c4cffa04207c29af8d9c3ab1f96eb738a416c
Lucho-ai <56923543+Lucho-ai@users.noreply.github.com>
2020-03-17 12:40:07 -0500

    Update p_porcent.c

--- p_porcent.c
+++ p_porcent.c
@@ -2,7 +2,8 @@
 
 /**
  * p_porcent - Prints a percent
- * @list: list of arguments                                                                                              * Return: Return the amount of characters printed.
+ * @list: list of arguments
+ * Return: Return the amount of characters printed.
  */
 int p_porcent(__attribute__((unused))va_list list)
 {
